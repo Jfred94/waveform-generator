6ec78f434c278e04a2a766367b52cf7db6034f1c
Emil IZAC <izac.emil@gmail.com>
2025-11-04 00:01:27 +0100

    cleaner code, all possible shapes

--- WaveformGenerator/Scenes/Game/TestVisualizer/test_visualizer.gd
+++ WaveformGenerator/Scenes/Game/TestVisualizer/test_visualizer.gd
@@ -49,61 +49,82 @@ func _process(delta: float) -> void:
 	connector_line.set_point_position(1, visualizer_circle.position)
 	connector_line2.set_point_position(1, visualizer_circle2.position)
 	
-	outline.texture = shape_sprites[shape_list.get_selected_items()[0]]
+	if (shape_sprites.size() > shape_list.get_selected_items()[0]):
+		outline.texture = shape_sprites[shape_list.get_selected_items()[0]]
+	else:
+		outline.texture = null
 
-# 0 = circle
-# 1 = small circle
-# 2 = square
-# 3 = diamond
-# 4 = inverted circle quadrants
-# 5 = oval
-# 6 = cut circle
-# 7 = rectangle
+ #0 = circle
+ #1 = small circle
+ #2 = square
+ #3 = diamond
+ #4 = inverted circle quadrants
+ #5 = oval (nevermind)
+ #6 = cut circle
+ #7 = rectangle
+ #8 = rectangle f
+ #9 = small square
+ #10 = small diamond
+ #11 = small rectangle
+ #12 = small rectangle f
+ #13 = small inv circle
+ #
+ #14 = circle + rectangle
+ #15 = circle + rectangle f
+ #16 = diamond + rectangle
+ #17 = diamond + rectangle f
+ #18 = rectangle + rectangle f
+ #19 = rectangle + inv circle
+ #20 = rectangle f + inv circle
+ #21 = inv circle + small circle
+ #22 = inv circle + small square
+ #23 = inv circle + small rectangle
+ #24 = inv circle + small rectangle f
+ #25 = small circle + small rectangle
+ #26 = small circle + small rectangle f
+ #27 = small diamond + small rectangle
+ #28 = small diamond + small rectangle f
+ #29 = small rectangle + small rectangle f
+ #30 = small rectangle + small inv circle
+ #31 = small rectangle f + small inv circle
+ #
+ #32 = circle - rectangle
+ #33 = circle - rectangle f
+ #34 = diamond - rectangle
+ #35 = diamond - rectangle f
+ #36 = rectangle - inv circle
+ #37 = rectangle f - inv circle
+ #38 = inv circle - small circle
+ #39 = inv circle - small square
+ #40 = inv circle - small rectangle
+ #41 = inv circle - small rectangle f
+ #42 = small circle - small rectangle
+ #43 = small circle - small rectangle f
+ #44 = small diamond - small rectangle
+ #45 = small diamond - small rectangle f
+ #46 = small rectangle - small rectangle f
+ #47 = small rectangle - small inv circle
+ #48 = small rectangle f - small inv circle
+
 func find_outside_position(angle: float, shape_type: int) ->  Vector2:
 	
 	if (shape_type == 0): # circle
-		return Vector2(-radius, 0).rotated(angle)
+		return get_circle_op(angle)
 		
 	elif (shape_type == 1): # small circle
-		return Vector2(-radius * 0.5, 0).rotated(angle)
+		return get_circle_op(angle) * 0.5
 		
 	elif (shape_type == 2): # square
-		if (angle >= PI / 4.0 && angle <= 3.0 * (PI / 4.0)):
-			return Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle) * 2.0, Vector2(-radius, -radius), Vector2(radius, -radius))
-		elif (angle > 3.0 * (PI / 4.0) && angle <= 5.0 * (PI / 4.0)):
-			return Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle) * 2.0, Vector2(radius, -radius), Vector2(radius, radius))
-		elif (angle > 5.0 * (PI / 4.0) && angle <= 7.0 * (PI / 4.0)):
-			return Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle) * 2.0, Vector2(radius, radius), Vector2(-radius, radius))
-		else:
-			return Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle) * 2.0, Vector2(-radius, radius), Vector2(-radius, -radius))
+		return get_square_op(angle)
 	
 	elif (shape_type == 3): # diamond
-		if (angle >= 0.0 && angle <= PI / 2.0):
-			return Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle) * 2.0, Vector2(-radius, 0.0), Vector2(0.0, -radius))
-		elif (angle > PI / 2.0 && angle <= PI):
-			return Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle) * 2.0, Vector2(0.0, -radius), Vector2(radius, 0.0))
-		elif (angle > PI && angle <= 3 * (PI / 2.0)):
-			return Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle) * 2.0, Vector2(radius, 0.0), Vector2(0.0, radius))
-		else:
-			return Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle) * 2.0, Vector2(0.0, radius), Vector2(-radius, 0.0))
+		return get_diamond_op(angle)
 	
 	elif (shape_type == 4): # inverted circle quadrants
-		if (angle >= 0.0 && angle <= PI / 2.0):
-			var intersection_value = Geometry2D.segment_intersects_circle(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle), Vector2(-radius, -radius), radius)
-			return Vector2(-radius, 0.0).rotated(angle) * intersection_value
-		elif (angle > PI / 2.0 && angle <= PI):
-			var intersection_value = Geometry2D.segment_intersects_circle(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle), Vector2(radius, -radius), radius)
-			return Vector2(-radius, 0.0).rotated(angle) * intersection_value
-		elif (angle > PI && angle <= 3 * (PI / 2.0)):
-			var intersection_value = Geometry2D.segment_intersects_circle(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle), Vector2(radius, radius), radius)
-			return Vector2(-radius, 0.0).rotated(angle) * intersection_value
-		else:
-			var intersection_value = Geometry2D.segment_intersects_circle(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle), Vector2(-radius, radius), radius)
-			return Vector2(-radius, 0.0).rotated(angle) * intersection_value
+		return get_invcircle_op(angle)
 	
-	elif (shape_type == 5): # oval
-		var circlePosition = Vector2(-radius, 0).rotated(angle)
-		return Vector2(circlePosition.x, circlePosition.y * 0.5)
+	elif (shape_type == 5): # oval (doesn't work)
+		return Vector2.ZERO
 	
 	elif (shape_type == 6): # cut circle
 		var circlePosition = Vector2(-radius, 0).rotated(angle)
@@ -116,8 +137,376 @@ func find_outside_position(angle: float, shape_type: int) ->  Vector2:
 				return Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle), Vector2(-radius, radius * 0.5), Vector2(radius, radius * 0.5))
 	
 	elif (shape_type == 7): # rectangle
-		return Vector2.ZERO
+		return get_rectangle_op(angle)
+	
+	elif (shape_type == 8): # rectangle f
+		return get_rectanglef_op(angle)
+	
+	elif (shape_type == 9): # small square
+		return get_square_op(angle) * 0.5
+	
+	elif (shape_type == 10): # small diamond
+		return get_diamond_op(angle) * 0.5
+	
+	elif (shape_type == 11): # small rectangle
+		return get_rectangle_op(angle) * 0.5
+	
+	elif (shape_type == 12): # small rectangle f
+		return get_rectanglef_op(angle) * 0.5
+	
+	elif (shape_type == 13): # small inv circle
+		return get_invcircle_op(angle) * 0.5
+	
+	elif (shape_type == 14): # circle + rectangle
+		var v1 = get_circle_op(angle)
+		var v2 = get_rectangle_op(angle)
+		if (v1.length() > v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 15): # circle + rectangle f
+		var v1 = get_circle_op(angle)
+		var v2 = get_rectanglef_op(angle)
+		if (v1.length() > v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 16): # diamond + rectangle
+		var v1 = get_diamond_op(angle)
+		var v2 = get_rectangle_op(angle)
+		if (v1.length() > v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 17): # diamond + rectangle f
+		var v1 = get_diamond_op(angle)
+		var v2 = get_rectanglef_op(angle)
+		if (v1.length() > v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 18): # rectangle + rectangle f
+		var v1 = get_rectangle_op(angle)
+		var v2 = get_rectanglef_op(angle)
+		if (v1.length() > v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 19): # rectangle + inv circle
+		var v1 = get_rectangle_op(angle)
+		var v2 = get_invcircle_op(angle)
+		if (v1.length() > v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 20): # rectangle f + inv circle
+		var v1 = get_rectanglef_op(angle)
+		var v2 = get_invcircle_op(angle)
+		if (v1.length() > v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 21): # inv circle + small circle
+		var v1 = get_invcircle_op(angle)
+		var v2 = get_circle_op(angle) * 0.5
+		if (v1.length() > v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 22): # inv circle + small square
+		var v1 = get_invcircle_op(angle)
+		var v2 = get_square_op(angle) * 0.5
+		if (v1.length() > v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 23): # inv circle + small rectangle
+		var v1 = get_invcircle_op(angle)
+		var v2 = get_rectangle_op(angle) * 0.5
+		if (v1.length() > v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 24): # inv circle + small rectangle f
+		var v1 = get_invcircle_op(angle)
+		var v2 = get_rectanglef_op(angle) * 0.5
+		if (v1.length() > v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 25): # small circle + small rectangle
+		var v1 = get_circle_op(angle) * 0.5
+		var v2 = get_rectangle_op(angle) * 0.5
+		if (v1.length() > v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 26): # small circle + small rectangle f
+		var v1 = get_circle_op(angle) * 0.5
+		var v2 = get_rectanglef_op(angle) * 0.5
+		if (v1.length() > v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 27): # small diamond + small rectangle
+		var v1 = get_diamond_op(angle) * 0.5
+		var v2 = get_rectangle_op(angle) * 0.5
+		if (v1.length() > v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 28): # small diamond + small rectangle f
+		var v1 = get_diamond_op(angle) * 0.5
+		var v2 = get_rectanglef_op(angle) * 0.5
+		if (v1.length() > v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 29): # small rectangle + small rectangle f
+		var v1 = get_rectangle_op(angle) * 0.5
+		var v2 = get_rectanglef_op(angle) * 0.5
+		if (v1.length() > v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 30): # small rectangle + small inv circle
+		var v1 = get_rectangle_op(angle) * 0.5
+		var v2 = get_invcircle_op(angle) * 0.5
+		if (v1.length() > v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 31): # small rectangle f + small inv circle
+		var v1 = get_rectanglef_op(angle) * 0.5
+		var v2 = get_invcircle_op(angle) * 0.5
+		if (v1.length() > v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 32): # circle - rectangle
+		var v1 = get_circle_op(angle)
+		var v2 = get_rectangle_op(angle)
+		if (v1.length() < v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 33): # circle - rectangle f
+		var v1 = get_circle_op(angle)
+		var v2 = get_rectanglef_op(angle)
+		if (v1.length() < v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 34): # diamond - rectangle
+		var v1 = get_diamond_op(angle)
+		var v2 = get_rectangle_op(angle)
+		if (v1.length() < v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 35): # diamond - rectangle f
+		var v1 = get_diamond_op(angle)
+		var v2 = get_rectanglef_op(angle)
+		if (v1.length() < v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 36): # rectangle - inv circle
+		var v1 = get_rectangle_op(angle)
+		var v2 = get_invcircle_op(angle)
+		if (v1.length() < v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 37): # rectangle f - inv circle
+		var v1 = get_rectanglef_op(angle)
+		var v2 = get_invcircle_op(angle)
+		if (v1.length() < v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 38): # inv circle - small circle
+		var v1 = get_invcircle_op(angle)
+		var v2 = get_circle_op(angle) * 0.5
+		if (v1.length() < v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 39): # inv circle - small square
+		var v1 = get_invcircle_op(angle)
+		var v2 = get_square_op(angle) * 0.5
+		if (v1.length() < v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 40): # inv circle - small rectangle
+		var v1 = get_invcircle_op(angle)
+		var v2 = get_rectangle_op(angle) * 0.5
+		if (v1.length() < v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 41): # inv circle - small rectangle f
+		var v1 = get_invcircle_op(angle)
+		var v2 = get_rectanglef_op(angle) * 0.5
+		if (v1.length() < v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 42): # small circle - small rectangle
+		var v1 = get_circle_op(angle) * 0.5
+		var v2 = get_rectangle_op(angle) * 0.5
+		if (v1.length() < v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 43): # small circle - small rectangle f
+		var v1 = get_circle_op(angle) * 0.5
+		var v2 = get_rectanglef_op(angle) * 0.5
+		if (v1.length() < v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 44): # small diamond - small rectangle
+		var v1 = get_diamond_op(angle) * 0.5
+		var v2 = get_rectangle_op(angle) * 0.5
+		if (v1.length() < v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 45): # small diamond - small rectangle f
+		var v1 = get_diamond_op(angle) * 0.5
+		var v2 = get_rectanglef_op(angle) * 0.5
+		if (v1.length() < v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 46): # small rectangle - small rectangle f
+		var v1 = get_rectangle_op(angle) * 0.5
+		var v2 = get_rectanglef_op(angle) * 0.5
+		if (v1.length() < v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 47): # small rectangle - small inv circle
+		var v1 = get_rectangle_op(angle) * 0.5
+		var v2 = get_invcircle_op(angle) * 0.5
+		if (v1.length() < v2.length()):
+			return v1
+		else:
+			return v2
+	
+	elif (shape_type == 48): # small rectangle f - small inv circle
+		var v1 = get_rectanglef_op(angle) * 0.5
+		var v2 = get_invcircle_op(angle) * 0.5
+		if (v1.length() < v2.length()):
+			return v1
+		else:
+			return v2
+	
 	
 	else:
 		return Vector2.ZERO
 	
+
+
+
+
+
+func get_circle_op(angle: float) ->  Vector2:
+	return Vector2(-radius, 0).rotated(angle)
+
+func get_square_op(angle: float) -> Vector2:
+	if (angle >= PI / 4.0 && angle <= 3.0 * (PI / 4.0)):
+		return Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle) * 2.0, Vector2(-radius, -radius), Vector2(radius, -radius))
+	elif (angle > 3.0 * (PI / 4.0) && angle <= 5.0 * (PI / 4.0)):
+		return Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle) * 2.0, Vector2(radius, -radius), Vector2(radius, radius))
+	elif (angle > 5.0 * (PI / 4.0) && angle <= 7.0 * (PI / 4.0)):
+		return Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle) * 2.0, Vector2(radius, radius), Vector2(-radius, radius))
+	else:
+		return Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle) * 2.0, Vector2(-radius, radius), Vector2(-radius, -radius))
+
+func get_diamond_op(angle: float) -> Vector2:
+	if (angle >= 0.0 && angle <= PI / 2.0):
+		return Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle) * 2.0, Vector2(-radius, 0.0), Vector2(0.0, -radius))
+	elif (angle > PI / 2.0 && angle <= PI):
+		return Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle) * 2.0, Vector2(0.0, -radius), Vector2(radius, 0.0))
+	elif (angle > PI && angle <= 3 * (PI / 2.0)):
+		return Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle) * 2.0, Vector2(radius, 0.0), Vector2(0.0, radius))
+	else:
+		return Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle) * 2.0, Vector2(0.0, radius), Vector2(-radius, 0.0))
+
+func get_invcircle_op(angle: float) -> Vector2:
+	if (angle >= 0.0 && angle <= PI / 2.0):
+		var intersection_value = Geometry2D.segment_intersects_circle(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle), Vector2(-radius, -radius), radius)
+		return Vector2(-radius, 0.0).rotated(angle) * intersection_value
+	elif (angle > PI / 2.0 && angle <= PI):
+		var intersection_value = Geometry2D.segment_intersects_circle(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle), Vector2(radius, -radius), radius)
+		return Vector2(-radius, 0.0).rotated(angle) * intersection_value
+	elif (angle > PI && angle <= 3 * (PI / 2.0)):
+		var intersection_value = Geometry2D.segment_intersects_circle(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle), Vector2(radius, radius), radius)
+		return Vector2(-radius, 0.0).rotated(angle) * intersection_value
+	else:
+		var intersection_value = Geometry2D.segment_intersects_circle(Vector2.ZERO, Vector2(-radius, 0.0).rotated(angle), Vector2(-radius, radius), radius)
+		return Vector2(-radius, 0.0).rotated(angle) * intersection_value
+
+func get_rectangle_op(angle: float) -> Vector2:
+	var topIntersection = Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius * 2.0, 0.0).rotated(angle), Vector2(-radius, -radius * 0.5), Vector2(radius, -radius * 0.5))
+	var rightIntersection = Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius * 2.0, 0.0).rotated(angle), Vector2(radius, -radius * 0.5), Vector2(radius, radius * 0.5))
+	var bottomIntersection = Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius * 2.0, 0.0).rotated(angle), Vector2(radius, radius * 0.5), Vector2(-radius, radius * 0.5))
+	var leftIntersection = Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius * 2.0, 0.0).rotated(angle), Vector2(-radius, radius * 0.5), Vector2(-radius, -radius * 0.5))
+	if (topIntersection != null):
+		return topIntersection
+	elif (rightIntersection != null):
+		return rightIntersection
+	elif (bottomIntersection != null):
+		return bottomIntersection
+	else:
+		return leftIntersection
+
+func get_rectanglef_op(angle: float) -> Vector2:
+	var topIntersection = Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius * 2.0, 0.0).rotated(angle), Vector2(-radius * 0.5, -radius), Vector2(radius * 0.5, -radius))
+	var rightIntersection = Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius * 2.0, 0.0).rotated(angle), Vector2(radius * 0.5, -radius), Vector2(radius * 0.5, radius))
+	var bottomIntersection = Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius * 2.0, 0.0).rotated(angle), Vector2(radius * 0.5, radius), Vector2(-radius * 0.5, radius))
+	var leftIntersection = Geometry2D.segment_intersects_segment(Vector2.ZERO, Vector2(-radius * 2.0, 0.0).rotated(angle), Vector2(-radius * 0.5, radius), Vector2(-radius * 0.5, -radius))
+	if (topIntersection != null):
+		return topIntersection
+	elif (rightIntersection != null):
+		return rightIntersection
+	elif (bottomIntersection != null):
+		return bottomIntersection
+	else:
+		return leftIntersection
